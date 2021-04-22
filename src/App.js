eda09357f308def96f2c3a68cb77fab3a2277a83
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-22 22:26:39 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, title: "Wiadomość numer 1", body: "Zawartość wiadomości numer 1 ..." },
+  { id: 2, title: "Wiadomość numer 2", body: "Zawartość wiadomości numer 2 ..." },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość wiadomości numer ${index} ...`,
+  });
+}, 4000);
+
+class App extends Component {
+  render() {
+    return <div className="App">Działa</div>;
+  }
 }
 
 export default App;
